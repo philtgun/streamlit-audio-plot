f728ba1451b71ac12553c24c98242a58b8397d43
Philip Tovstogan <phil.tgun@gmail.com>
2023-09-13 16:48:27 +0200

    feat: data marshalling, less looking like plot

--- audio_plot/__init__.py
+++ audio_plot/__init__.py
@@ -17,19 +17,58 @@ else:
     build_dir = os.path.join(parent_dir, "frontend-react/build")
     _component_func = components.declare_component("audio_plot", path=build_dir)
 
-def audio_plot(key=None):
-    """Create a new instance of "my_component".
+def audio_plot(
+        embeddings: np.ndarray | list[list] | pd.DataFrame,
+        urls: list[str] | pd.Series,
+        labels: list[str] | pd.Series = None,
+        height: int = 600,
+        volume: float = 0.4,
+        key=None
+    ):
+    """Create a new instance of "audio_plot".
 
     Parameters
     ----------
 
 
     """
-    urls = [
-            "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3#t=0.0,3.0",
-            "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3#t=60.0,63.0",
-        ]
-    return _component_func(x=[0,1], y=[1,-2], urls=urls, height=800, key=key)
+    # transform embeddings to list of lists
+    if type(embeddings) is pd.DataFrame:
+        embeddings = embeddings.to_numpy()
+    
+    if type(embeddings) is list:
+        embeddings = np.array(embeddings)
+
+    if type(embeddings) is np.ndarray:
+        embeddings = embeddings.T.tolist()        
+
+    if len(embeddings) != 2:
+        raise ValueError("Embeddings must be a 2D array with shape=(2,n), DataFrame with two columns or list of two lists")
+
+    if len(embeddings[0]) != len(embeddings[1]):
+        raise ValueError(f"Lists in embeddings must have the same length (0: {embeddings[0]} vs 1: {embeddings[1]})")
+    
+    # transform urls to list
+    if type(urls) is pd.Series:
+        urls = urls.to_list()
+
+    # transform labels to list
+    if type(labels) is pd.Series:
+        labels = labels.to_list()
+
+    # check lengths
+    total = len(embeddings[0])
+    if total != len(urls):
+        raise ValueError(f"Embeddings and urls must have the same length (embeddings: {len(embeddings[0])} vs urls: {len(urls)})")
+    
+    return _component_func(
+        embeddings=embeddings,
+        urls=urls, 
+        labels=labels,
+        height=height,
+        volume=volume,
+        key=key
+    )
 
 
 if not _RELEASE:
@@ -37,4 +76,13 @@ if not _RELEASE:
 
     # Create an instance of our component with a constant `name` arg, and
     # print its output value.
-    audio_plot()
+    _urls = [
+        "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3#t=0.0,3.0",
+        "https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3#t=60.0,63.0",
+    ]
+    
+    audio_plot(
+        embeddings=[[1,1], [2,2]], 
+        urls=_urls,
+        labels=["Track 1", "Track 2"],
+    )
--- audio_plot/frontend/src/AudioPlot.tsx
+++ audio_plot/frontend/src/AudioPlot.tsx
@@ -1,15 +1,11 @@
 import {
-  ArrowTable,
   StreamlitComponentBase,
   withStreamlitConnection,
 } from "streamlit-component-lib"
 import React, { ReactNode, createRef } from "react"
 import Plot from 'react-plotly.js'
-// import ReactAudioPlayer from 'react-audio-player'
 
-interface State {
-
-}
+interface State {}
 
 /**
  * This is a React-based component template. The `render()` function is called
@@ -23,24 +19,17 @@ class AudioPlot extends StreamlitComponentBase<State> {
   //   this.audioPlayerRef = React.createRef();
   // }
 
-  public state = {
-    urls: [],
-  }
+  public state = {}
 
   public render = (): ReactNode => {
     const { args } = this.props
-
-    const urls = args["urls"]
-    this.setState({ urls })
-
-    const height = args["height"] ? args["height"] : 600
 
     // Streamlit sends us a theme object via props that we can use to ensure
     // that our component has visuals that match the active theme in a
     // streamlit app.
     const { theme } = this.props
     const style: React.CSSProperties = {
-      height: height,
+      height: args.height,
     }
 
     // Maintain compatibility with older versions of Streamlit that don't send
@@ -54,17 +43,36 @@ class AudioPlot extends StreamlitComponentBase<State> {
         <Plot
           data={[
             {
-              x: args["x"],
-              y: args["y"],
+              x: args.embeddings[0],
+              y: args.embeddings[1],
               type: 'scatter',
               mode: 'markers',
+              text: args.labels,
+              hoverinfo: 'text',
             },
           ]}
           layout={{
-            grid: {
-              xaxes: [],
-              yaxes: [],
+            margin: {
+              b: 0,
+              t: 0,
+              l: 0,
+              r: 0,
             },
+            xaxis: {
+              showgrid: false,
+              zeroline: false,
+              visible: false,
+            },
+            yaxis: {
+              showgrid: false,
+              zeroline: false,
+              visible: false,
+            },
+            paper_bgcolor: 'rgba(0,0,0,0.05)',
+            plot_bgcolor: 'rgba(0,0,0,0.05)',
+          }}
+          config={{
+            scrollZoom: true,
           }}
           onHover={this._onHover}
           onClick={this._onClick}
@@ -83,7 +91,7 @@ class AudioPlot extends StreamlitComponentBase<State> {
     const audioElement: any = this.audioPlayerRef.current
     if (audioElement) {
       audioElement.src = url
-      audioElement.volume = 0.4
+      audioElement.volume = this.props.args.volume
       audioElement.play()
     }
   }
@@ -94,7 +102,7 @@ class AudioPlot extends StreamlitComponentBase<State> {
 
   private _onHover = (event: any) => {
     console.log("hover", event)
-    const url = this.state.urls[event.points[0].pointIndex]
+    const url = this.props.args.urls[event.points[0].pointIndex]
     this.playAudio(url)
   }
 
