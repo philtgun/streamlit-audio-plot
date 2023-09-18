724ab423ff7dbe460f2da6c880774cf5228dcb1f
Philip Tovstogan <phil.tgun@gmail.com>
2023-09-18 17:55:02 +0200

    ci: setup pypi publishing, works on test pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-audio-plot",
-    version="0.1.1",
+    version="0.0.1",
     author="Philip Tovstogan",
     author_email="phil.tovstogan@gmail.com",
     description="Streamlit component that allows you to do plot audio embeddings as a plotly figure with source audio playback on hover or click.",
